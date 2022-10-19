a7380d23707f222ba35ff8f7f611ba003129b89d
Luis Amesty <luisamesty@gmail.com>
2022-10-19 22:36:39 +0200

    MAC and LINUX Builds

---
+++ buildMac.sh
@@ -0,0 +1,28 @@
+# DELETE old Build plugins
+rm build.plugins/*.jar
+
+# COPY MAC target file
+cp org.amerpsoft.com.idempiere.p2.targetplatform/Mac.target org.amerpsoft.com.idempiere.p2.targetplatform/org.amerpsoft.com.idempiere.p2.targetplatform.target
+
+#  COMMAND TO Build Maven Plugins
+mvn verify -Didempiere.target=org.amerpsoft.com.idempiere.p2.targetplatform -X
+
+# COPY Build plugins to build.plugins directory
+cp org.amerpsoft.com.idempiere.p2.site/target/repository/plugins/*.jar build.plugins/
+
+
+# NOTE: Compilation is configured for local MAC OS development environment
+# Compiling in a different server setup you need to change relativePath and Location.
+#
+# 1. Change Relative Path in files:
+#   org.amerpsoft.idempiere.p2.site/pom.xml
+#   org.amerpsoft.idempiere.p2.targetplatform/pom.xml
+#   org.amerpsoft.idempiere.editors-com/pom.xml
+#   org.amerpsoft.idempiere.themes/pom.xml
+#  Relative Path is set as: 
+#  <relativePath>../../idempiere/org.idempiere.parent/pom.xml</relativePath>
+#
+# 2. Change the repository location in file:
+#   org.amerpsoft.com.idempiere.p2.targetplatform/org.amerpsoft.com.idempiere.p2.targetplatform.target
+#   Repository Location is set as: 
+# 		<repository location="file:///Users/luisamesty/sources/idempiere/org.idempiere.p2/target/repository"/>
