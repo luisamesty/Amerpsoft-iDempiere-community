ca6f44c9dd74469b3938e82868bb14b1e23d9d72
luisamesty <luisamesty@gmail.com>
2024-04-17 18:06:18 +0200

    LPY115 Changes on demografia Venezolana Pack

--- org.amerpsoft.com.idempiere.editors-com/sql/LVE/delete_demografia_venezuela.sql
+++ org.amerpsoft.com.idempiere.editors-com/sql/LVE/delete_demografia_venezuela.sql
@@ -13,3 +13,6 @@ COMMIT;
 
 DELETE FROM adempiere.C_region  WHERE C_country_id= 339;
 COMMIT;
+
+DELETE FROM c_community  WHERE C_country_id= 339 
+AND c_community_id IN (33901,33902,33903,33904,33905,33906,33907,33908,33909);
