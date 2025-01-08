c4ae72587c3e5e29e68577fa049d150de3500a52
luisamesty <luisamesty@luisamesty@gmail.com>
2025-01-08 08:57:56 +0100

    release-11_LPY Leaves Types Table updated.  Calendar o Business days logic for days calculation

--- org.amerpsoft.com.idempiere.personnelpayroll/src/org/amerp/amnmodel/X_AMN_Leaves_Types.java
+++ org.amerpsoft.com.idempiere.personnelpayroll/src/org/amerp/amnmodel/X_AMN_Leaves_Types.java
@@ -33,7 +33,7 @@ public class X_AMN_Leaves_Types extends PO implements I_AMN_Leaves_Types, I_Pers
 	/**
 	 *
 	 */
-	private static final long serialVersionUID = 20241220L;
+	private static final long serialVersionUID = 20250108L;
 
     /** Standard Constructor */
     public X_AMN_Leaves_Types (Properties ctx, int AMN_Leaves_Types_ID, String trxName)
@@ -172,6 +172,27 @@ public class X_AMN_Leaves_Types extends PO implements I_AMN_Leaves_Types, I_Pers
 		return (String)get_Value(COLUMNNAME_AMN_Leaves_Types_UU);
 	}
 
+	/** Business Days = B */
+	public static final String DAYSMODE_BusinessDays = "B";
+	/** Calendar Days = C */
+	public static final String DAYSMODE_CalendarDays = "C";
+	/** Set Days Calculation Mode.
+		@param DaysMode Days Calculation Mode using Calendar  or Business Days
+	*/
+	public void setDaysMode (String DaysMode)
+	{
+
+		set_Value (COLUMNNAME_DaysMode, DaysMode);
+	}
+
+	/** Get Days Calculation Mode.
+		@return Days Calculation Mode using Calendar  or Business Days
+	  */
+	public String getDaysMode()
+	{
+		return (String)get_Value(COLUMNNAME_DaysMode);
+	}
+
 	/** Set Default Days.
 		@param DefaultDays Default Days Quantity
 	*/
@@ -207,6 +228,24 @@ public class X_AMN_Leaves_Types extends PO implements I_AMN_Leaves_Types, I_Pers
 		return (String)get_Value(COLUMNNAME_Description);
 	}
 
+	/** Set Hours per day in a Shift.
+		@param HoursDay Hours per day in a Shift
+	*/
+	public void setHoursDay (BigDecimal HoursDay)
+	{
+		set_Value (COLUMNNAME_HoursDay, HoursDay);
+	}
+
+	/** Get Hours per day in a Shift.
+		@return Hours per day in a Shift	  */
+	public BigDecimal getHoursDay()
+	{
+		BigDecimal bd = (BigDecimal)get_Value(COLUMNNAME_HoursDay);
+		if (bd == null)
+			 return Env.ZERO;
+		return bd;
+	}
+
 	/** Set Lead Time.
 		@param LeadTime Lead TimeLead time for a request
 	*/
@@ -221,6 +260,25 @@ public class X_AMN_Leaves_Types extends PO implements I_AMN_Leaves_Types, I_Pers
 	public int getLeadTime()
 	{
 		Integer ii = (Integer)get_Value(COLUMNNAME_LeadTime);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Maximun Days To Pay.
+		@param MaximunDaysToPay Maximun Days To Pay witin a month
+	*/
+	public void setMaximunDaysToPay (int MaximunDaysToPay)
+	{
+		set_Value (COLUMNNAME_MaximunDaysToPay, Integer.valueOf(MaximunDaysToPay));
+	}
+
+	/** Get Maximun Days To Pay.
+		@return Maximun Days To Pay witin a month
+	  */
+	public int getMaximunDaysToPay()
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_MaximunDaysToPay);
 		if (ii == null)
 			 return 0;
 		return ii.intValue();
