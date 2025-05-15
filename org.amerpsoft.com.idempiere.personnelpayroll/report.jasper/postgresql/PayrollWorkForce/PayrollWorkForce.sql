193b1430cb1ebb7aae8aca047047796636e7cb3c
luisamesty <luisamesty@luisamesty@gmail.com>
2025-05-15 10:05:15 +0200

    release-11_LPY-36 Reportes de Contabilidad de Nominas. Pack Updated

--- org.amerpsoft.com.idempiere.personnelpayroll/report.jasper/postgresql/PayrollWorkForce/PayrollWorkForce.sql
+++ org.amerpsoft.com.idempiere.personnelpayroll/report.jasper/postgresql/PayrollWorkForce/PayrollWorkForce.sql
@@ -1,6 +1,104 @@
 -- Payroll Workforce
 -- PayrollWorkforce.jrxml
 -- optmone='A' reemplazado por pydma.accounttype ='E'
+WITH Concept_accounting AS (
+	SELECT * FROM (
+	    -- DEBIT ACCOUNTS
+	    SELECT ctys.amn_concept_types_id, ctys.calcorder, ctys.value, ctys.name, ctys.optmode, ctysa.c_acctschema_id,
+	        'DB' AS account_sign, 'D' AS workforce,
+	        cedb.C_ElementValue_ID AS account_id, cedb.value AS account_value,
+	        cedb.name AS account_name, cedb.accounttype
+	    FROM AMN_Concept_Types ctys
+	    LEFT JOIN AMN_Concept_Types_Acct ctysa ON ctysa.amn_concept_types_id = ctys.amn_concept_types_id
+	    LEFT JOIN C_ValidCombination vacodb ON vacodb.C_ValidCombination_ID = ctysa.amn_deb_dw_acct
+	    LEFT JOIN C_ElementValue cedb ON cedb.C_ElementValue_ID = vacodb.Account_ID
+	
+	    UNION
+	    SELECT ctys.amn_concept_types_id, ctys.calcorder, ctys.value, ctys.name, ctys.optmode, ctysa.c_acctschema_id,
+	        'DB', 'I',
+	        cedb.C_ElementValue_ID, cedb.value, cedb.name, cedb.accounttype
+	    FROM AMN_Concept_Types ctys
+	    LEFT JOIN AMN_Concept_Types_Acct ctysa ON ctysa.amn_concept_types_id = ctys.amn_concept_types_id
+	    LEFT JOIN C_ValidCombination vacodb ON vacodb.C_ValidCombination_ID = ctysa.amn_deb_iw_acct
+	    LEFT JOIN C_ElementValue cedb ON cedb.C_ElementValue_ID = vacodb.Account_ID
+	
+	    UNION
+	    SELECT ctys.amn_concept_types_id, ctys.calcorder, ctys.value, ctys.name, ctys.optmode, ctysa.c_acctschema_id,
+	        'DB', 'M',
+	        cedb.C_ElementValue_ID, cedb.value, cedb.name, cedb.accounttype
+	    FROM AMN_Concept_Types ctys
+	    LEFT JOIN AMN_Concept_Types_Acct ctysa ON ctysa.amn_concept_types_id = ctys.amn_concept_types_id
+	    LEFT JOIN C_ValidCombination vacodb ON vacodb.C_ValidCombination_ID = ctysa.amn_deb_mw_acct
+	    LEFT JOIN C_ElementValue cedb ON cedb.C_ElementValue_ID = vacodb.Account_ID
+	
+	    UNION
+	    SELECT ctys.amn_concept_types_id, ctys.calcorder, ctys.value, ctys.name, ctys.optmode, ctysa.c_acctschema_id,
+	        'DB', 'S',
+	        cedb.C_ElementValue_ID, cedb.value, cedb.name, cedb.accounttype
+	    FROM AMN_Concept_Types ctys
+	    LEFT JOIN AMN_Concept_Types_Acct ctysa ON ctysa.amn_concept_types_id = ctys.amn_concept_types_id
+	    LEFT JOIN C_ValidCombination vacodb ON vacodb.C_ValidCombination_ID = ctysa.amn_deb_sw_acct
+	    LEFT JOIN C_ElementValue cedb ON cedb.C_ElementValue_ID = vacodb.Account_ID
+	
+	    UNION
+	    SELECT ctys.amn_concept_types_id, ctys.calcorder, ctys.value, ctys.name, ctys.optmode, ctysa.c_acctschema_id,
+	        'DB', 'A',
+	        cedb.C_ElementValue_ID, cedb.value, cedb.name, cedb.accounttype
+	    FROM AMN_Concept_Types ctys
+	    LEFT JOIN AMN_Concept_Types_Acct ctysa ON ctysa.amn_concept_types_id = ctys.amn_concept_types_id
+	    LEFT JOIN C_ValidCombination vacodb ON vacodb.C_ValidCombination_ID = ctysa.amn_deb_acct
+	    LEFT JOIN C_ElementValue cedb ON cedb.C_ElementValue_ID = vacodb.Account_ID
+	
+	    -- CREDIT ACCOUNTS
+	    UNION
+	    SELECT ctys.amn_concept_types_id, ctys.calcorder, ctys.value, ctys.name, ctys.optmode, ctysa.c_acctschema_id,
+	        'CR', 'D',
+	        cecr.C_ElementValue_ID, cecr.value, cecr.name, cecr.accounttype
+	    FROM AMN_Concept_Types ctys
+	    LEFT JOIN AMN_Concept_Types_Acct ctysa ON ctysa.amn_concept_types_id = ctys.amn_concept_types_id
+	    LEFT JOIN C_ValidCombination vacocr ON vacocr.C_ValidCombination_ID = ctysa.amn_cre_dw_acct
+	    LEFT JOIN C_ElementValue cecr ON cecr.C_ElementValue_ID = vacocr.Account_ID
+	
+	    UNION
+	    SELECT ctys.amn_concept_types_id, ctys.calcorder, ctys.value, ctys.name, ctys.optmode, ctysa.c_acctschema_id,
+	        'CR', 'I',
+	        cecr.C_ElementValue_ID, cecr.value, cecr.name, cecr.accounttype
+	    FROM AMN_Concept_Types ctys
+	    LEFT JOIN AMN_Concept_Types_Acct ctysa ON ctysa.amn_concept_types_id = ctys.amn_concept_types_id
+	    LEFT JOIN C_ValidCombination vacocr ON vacocr.C_ValidCombination_ID = ctysa.amn_cre_iw_acct
+	    LEFT JOIN C_ElementValue cecr ON cecr.C_ElementValue_ID = vacocr.Account_ID
+	
+	    UNION
+	    SELECT ctys.amn_concept_types_id, ctys.calcorder, ctys.value, ctys.name, ctys.optmode, ctysa.c_acctschema_id,
+	        'CR', 'M',
+	        cecr.C_ElementValue_ID, cecr.value, cecr.name, cecr.accounttype
+	    FROM AMN_Concept_Types ctys
+	    LEFT JOIN AMN_Concept_Types_Acct ctysa ON ctysa.amn_concept_types_id = ctys.amn_concept_types_id
+	    LEFT JOIN C_ValidCombination vacocr ON vacocr.C_ValidCombination_ID = ctysa.amn_cre_mw_acct
+	    LEFT JOIN C_ElementValue cecr ON cecr.C_ElementValue_ID = vacocr.Account_ID
+	
+	    UNION
+	    SELECT ctys.amn_concept_types_id, ctys.calcorder, ctys.value, ctys.name, ctys.optmode, ctysa.c_acctschema_id,
+	        'CR', 'S',
+	        cecr.C_ElementValue_ID, cecr.value, cecr.name, cecr.accounttype
+	    FROM AMN_Concept_Types ctys
+	    LEFT JOIN AMN_Concept_Types_Acct ctysa ON ctysa.amn_concept_types_id = ctys.amn_concept_types_id
+	    LEFT JOIN C_ValidCombination vacocr ON vacocr.C_ValidCombination_ID = ctysa.amn_cre_sw_acct
+	    LEFT JOIN C_ElementValue cecr ON cecr.C_ElementValue_ID = vacocr.Account_ID
+	
+	    UNION
+	    SELECT ctys.amn_concept_types_id, ctys.calcorder, ctys.value, ctys.name, ctys.optmode, ctysa.c_acctschema_id,
+	        'CR', 'A',
+	        cecr.C_ElementValue_ID, cecr.value, cecr.name, cecr.accounttype
+	    FROM AMN_Concept_Types ctys
+	    LEFT JOIN AMN_Concept_Types_Acct ctysa ON ctysa.amn_concept_types_id = ctys.amn_concept_types_id
+	    LEFT JOIN C_ValidCombination vacocr ON vacocr.C_ValidCombination_ID = ctysa.amn_cre_acct
+	    LEFT JOIN C_ElementValue cecr ON cecr.C_ElementValue_ID = vacocr.Account_ID
+	  ) AS accountsall
+	  WHERE accountsall.accounttype='E'
+	  ORDER BY calcorder, workforce, account_sign DESC
+) 
+-- MAIN QUERY
 SELECT * FROM
 (
 	-- REPORT HEADER
@@ -20,22 +118,21 @@ SELECT * FROM
 		CASE WHEN ($P{AD_Org_ID} IS NULL OR $P{AD_Org_ID} = 0) THEN img1.binarydata ELSE img2.binarydata END as rep_logo,
 	    CASE WHEN  org.ad_client_id = $P{AD_Client_ID}   AND ($P{AD_Org_ID} IS NULL OR $P{AD_Org_ID} = 0) THEN 1
 	             WHEN  org.ad_client_id = $P{AD_Client_ID}  AND org.ad_org_id= $P{AD_Org_ID}  THEN 1
-	             ELSE 0 END as imp_header,
-	    -- CURRENCY
-		CONCAT(curr.iso_code,'-',currt.cursymbol,'-',COALESCE(currt.description,curr.iso_code,curr.cursymbol,'')) as moneda
+	             ELSE 0 END as imp_header
 	FROM adempiere.ad_client as cli
 		 INNER JOIN adempiere.ad_org as org ON (org.ad_client_id = cli.ad_client_id)
 		 INNER JOIN adempiere.ad_clientinfo as cliinfo ON (cli.ad_client_id = cliinfo.ad_client_id)
 		  LEFT JOIN adempiere.ad_image as img1 ON (cliinfo.logoreport_id = img1.ad_image_id)
 		 INNER JOIN adempiere.ad_orginfo as orginfo ON (org.ad_org_id = orginfo.ad_org_id)
 		  LEFT JOIN adempiere.ad_image as img2 ON (orginfo.logo_id = img2.ad_image_id)
-		  LEFT JOIN c_currency curr on curr.c_currency_id = $P{C_Currency_ID}
-          LEFT JOIN c_currency_trl currt on curr.c_currency_id = currt.c_currency_id and currt.ad_language = (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID=$P{AD_Client_ID})
 	WHERE cli.ad_client_id = $P{AD_Client_ID} AND CASE WHEN ($P{AD_Org_ID} IS NULL OR $P{AD_Org_ID} = 0 OR org.ad_org_id = $P{AD_Org_ID} ) THEN 1 = 1 ELSE 1 = 0 END
 ) as header_info
 FULL JOIN
 (
 SELECT 
+	-- C_AcctSchema
+	pydma.c_acctschema_id,
+	pydma.schema_name,
     -- CURRENCY
     curr1.iso_code as iso_code1,
     currt1.cursymbol as cursymbol1,
@@ -82,9 +179,9 @@ SELECT
    -- C_YEAR 
    year2.c_year_id,  year2.periodos
     FROM AMN_Period tiper
-    LEFT JOIN C_Period per ON (per.c_period_id = tiper.c_period_id )
-    LEFT JOIN C_Year year ON (year.C_Year_ID = per.C_Year_ID )
-    LEFT JOIN (
+    INNER JOIN C_Period per ON (per.c_period_id = tiper.c_period_id )
+    INNER JOIN C_Year year ON (year.C_Year_ID = per.C_Year_ID )
+    INNER JOIN (
 		SELECT DISTINCT ON (per2.C_Year_ID) 
 		per2.C_Year_ID,
 		CONCAT(MAX(per2.periodtxt01)::text, ' ', MAX(per2.periodtxt02)::text, ' ', MAX(per2.periodtxt03)::text, ' ', MAX(per2.periodtxt04)::text,
@@ -111,8 +208,8 @@ SELECT
 		FROM C_Period cper WHERE cper.c_period_id >=  $P{C_PeriodIni_ID} AND cper.c_period_id <=  $P{C_PeriodEnd_ID} AND cper.c_year_id = $P{C_Year_ID}
 		) as per2
 		GROUP BY per2.C_Year_ID
-	    ) year2 ON ( year2.c_year_id = year.c_year_id)
-    LEFT JOIN adempiere.amn_payroll as pyr ON (pyr.amn_period_id = tiper.amn_period_id)
+	) year2 ON ( year2.c_year_id = year.c_year_id)
+    INNER JOIN adempiere.amn_payroll as pyr ON (pyr.amn_period_id = tiper.amn_period_id)
     LEFT JOIN (
 		-- DEBITS
 		SELECT
@@ -141,6 +238,9 @@ SELECT
 		ctyacc.value as concept_value,
 		ctyacc.name as concept_name,
 		ctyacc.optmode,
+		-- C_AcctSchema
+		acsch.c_acctschema_id,
+		acsch.name AS schema_name,
 		-- Account 
 		ctyacc.account_sign,
 		ctyacc.account_value,
@@ -148,80 +248,47 @@ SELECT
 		ctyacc.accounttype,
 		CASE WHEN ctyacc.account_sign ='DB' AND prdet.amountallocated <> 0 THEN 
 				--prdet.amountallocated 
-				currencyConvert(pay.amountallocated,pay.c_currency_id, $P{C_Currency_ID}, pay.dateacct, pay.C_ConversionType_ID, pay.AD_Client_ID, pay.AD_Org_ID ) 
+				currencyConvert(pay.amountallocated,pay.c_currency_id, acsch.c_currency_id, pay.dateacct, pay.C_ConversionType_ID, pay.AD_Client_ID, pay.AD_Org_ID ) 
 		     WHEN ctyacc.account_sign ='DB' AND prdet.amountdeducted <> 0 THEN 
 		     	--prdet.amountdeducted 
-				currencyConvert(pay.amountdeducted,pay.c_currency_id, $P{C_Currency_ID}, pay.dateacct, pay.C_ConversionType_ID, pay.AD_Client_ID, pay.AD_Org_ID ) 
+				currencyConvert(pay.amountdeducted,pay.c_currency_id, acsch.c_currency_id, pay.dateacct, pay.C_ConversionType_ID, pay.AD_Client_ID, pay.AD_Org_ID ) 
 		     WHEN ctyacc.account_sign ='DB' AND (prdet.amountallocated = 0 AND prdet.amountdeducted = 0 ) THEN 
 		     	--prdet.amountcalculated
-		     	currencyConvert(pay.amountcalculated,pay.c_currency_id, $P{C_Currency_ID}, pay.dateacct, pay.C_ConversionType_ID, pay.AD_Client_ID, pay.AD_Org_ID ) 
+		     	currencyConvert(pay.amountcalculated,pay.c_currency_id, acsch.c_currency_id, pay.dateacct, pay.C_ConversionType_ID, pay.AD_Client_ID, pay.AD_Org_ID ) 
 		ELSE  0 END as db_acct,
 		CASE WHEN ctyacc.account_sign ='CR' AND prdet.amountallocated <> 0 THEN 
 				--prdet.amountallocated 
-		     	currencyConvert(pay.amountallocated,pay.c_currency_id, $P{C_Currency_ID}, pay.dateacct, pay.C_ConversionType_ID, pay.AD_Client_ID, pay.AD_Org_ID ) 
+		     	currencyConvert(pay.amountallocated,pay.c_currency_id, acsch.c_currency_id, pay.dateacct, pay.C_ConversionType_ID, pay.AD_Client_ID, pay.AD_Org_ID ) 
 		     WHEN ctyacc.account_sign ='CR' AND prdet.amountdeducted <> 0 THEN 
 		     	--prdet.amountdeducted 
-		     	currencyConvert(pay.amountdeducted,pay.c_currency_id, $P{C_Currency_ID}, pay.dateacct, pay.C_ConversionType_ID, pay.AD_Client_ID, pay.AD_Org_ID ) 
+		     	currencyConvert(pay.amountdeducted,pay.c_currency_id, acsch.c_currency_id, pay.dateacct, pay.C_ConversionType_ID, pay.AD_Client_ID, pay.AD_Org_ID ) 
 		     WHEN ctyacc.account_sign ='CR' AND (prdet.amountallocated = 0 AND prdet.amountdeducted = 0 ) THEN 
 		     	--prdet.amountcalculated
-		     	currencyConvert(pay.amountcalculated,pay.c_currency_id, $P{C_Currency_ID}, pay.dateacct, pay.C_ConversionType_ID, pay.AD_Client_ID, pay.AD_Org_ID ) 
+		     	currencyConvert(pay.amountcalculated,pay.c_currency_id, acsch.c_currency_id, pay.dateacct, pay.C_ConversionType_ID, pay.AD_Client_ID, pay.AD_Org_ID ) 
 		ELSE  0 END  as cr_acct
 		-- Amount
-		--prdet.amountallocated,
-		--prdet.amountdeducted,
-		--prdet.amountcalculated
-		FROM AMN_Payroll_Detail prdet
-		LEFT JOIN AMN_Concept_Types_Proc ctyp ON (ctyp.AMN_Concept_Types_Proc_ID = prdet.AMN_Concept_Types_Proc_ID)
-		LEFT JOIN 
-		(--DEBIT ACCOUNT
-		 SELECT
-			ctys.amn_concept_types_id as amn_concept_types_id, 
-			ctys.calcorder as calcorder,
-			ctys.value,
-			ctys.name,
-			ctys.optmode,
-			'DB' as account_sign,
-			cedb.C_ElementValue_ID as Account_ID,
-			cedb.value as account_value,
-			cedb.name as account_name,
-			cedb.accounttype as accounttype
-			FROM AMN_Concept_Types ctys 
-			LEFT JOIN C_ValidCombination vacodb ON ( vacodb.C_ValidCombination_ID = ctys.AMN_Deb_Acct)
-			LEFT JOIN C_ElementValue cedb ON (cedb.C_ElementValue_ID = vacodb.Account_ID)
-			-- CREDIT ACCOUNT
-		  UNION
-		  SELECT
-			ctys.amn_concept_types_id as amn_concept_types_id, 
-			ctys.calcorder  as calcorder,
-			ctys.value,
-			ctys.name,
-			ctys.optmode,
-			'CR' as account_sign,
-			cecr.C_ElementValue_ID as Account_ID,
-			cecr.value as cr_value,
-			cecr.name as cr_name,
-			cecr.accounttype as accounttype
-			FROM AMN_Concept_Types ctys 
-			LEFT JOIN C_ValidCombination vacocr ON ( vacocr.C_ValidCombination_ID = ctys.AMN_Cre_Acct)
-			LEFT JOIN C_ElementValue cecr ON (cecr.C_ElementValue_ID = vacocr.Account_ID)
-			-- ORDER
-			ORDER BY Calcorder, account_sign DESC
-	           ) as ctyacc ON ( ctyacc.AMN_Concept_Types_ID = ctyp.AMN_Concept_Types_ID)
-	       LEFT JOIN AMN_Payroll pay ON ( pay.AMN_Payroll_ID = prdet.amn_payroll_id )
-		   LEFT JOIN AMN_Employee emp ON ( emp.amn_employee_id = pay.amn_employee_id)
-		   LEFT JOIN C_Activity act ON ( act.C_Activity_ID = emp.C_Activity_ID )
-		   LEFT JOIN AMN_JobTitle jobt ON (jobt.AMN_JobTitle_ID = emp.AMN_JobTitle_ID ) 
-		) as pydma ON ( pydma.amn_payroll_id = pyr.amn_payroll_id)
-       LEFT JOIN adempiere.ad_org as org ON (org.ad_org_id = pydma.ad_org_id)
-       INNER JOIN adempiere.ad_client as cli ON (org.ad_client_id = cli.ad_client_id)
-       INNER JOIN adempiere.ad_clientinfo as cliinfo ON (cli.ad_client_id = cliinfo.ad_client_id)
-       INNER JOIN adempiere.ad_orginfo as orginfo ON (org.ad_org_id = orginfo.ad_org_id)
-       LEFT JOIN adempiere.ad_image AS IMG ON (cliinfo.logoreport_id = IMG.ad_image_id)
-       LEFT JOIN c_currency curr1 on pyr.c_currency_id = curr1.c_currency_id
-       LEFT JOIN c_currency_trl currt1 on curr1.c_currency_id = currt1.c_currency_id and currt1.ad_language = (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID=$P{AD_Client_ID}) 
-       LEFT JOIN c_currency curr2 on curr2.c_currency_id = $P{C_Currency_ID}
-       LEFT JOIN c_currency_trl currt2 on curr2.c_currency_id = currt2.c_currency_id and currt2.ad_language = (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID=$P{AD_Client_ID})
-WHERE  pydma.ad_client_id = $P{AD_Client_ID} 
+		FROM adempiere.AMN_Payroll_Detail prdet
+		INNER JOIN adempiere.AMN_Concept_Types_Proc ctyp ON (ctyp.AMN_Concept_Types_Proc_ID = prdet.AMN_Concept_Types_Proc_ID)
+		INNER JOIN adempiere.AMN_Payroll pay ON ( pay.AMN_Payroll_ID = prdet.amn_payroll_id )
+		INNER JOIN adempiere.AMN_Employee emp ON ( emp.amn_employee_id = pay.amn_employee_id)
+		INNER JOIN adempiere.AMN_JobTitle jobt ON (jobt.AMN_JobTitle_ID = emp.AMN_JobTitle_ID ) 
+		INNER JOIN adempiere.c_acctschema acsch ON (acsch.ad_client_id = pay.ad_client_id AND acsch.c_acctschema_id = $P{C_AcctSchema_ID}  )
+		LEFT JOIN Concept_accounting as ctyacc ON ( ctyacc.c_acctschema_id = acsch.c_acctschema_id AND  ctyacc.AMN_Concept_Types_ID = ctyp.AMN_Concept_Types_ID AND ctyacc.workforce = jobt.workforce )
+		LEFT JOIN C_Activity act ON ( act.C_Activity_ID = emp.C_Activity_ID )
+	   ) as pydma ON ( pydma.amn_payroll_id = pyr.amn_payroll_id)
+   INNER JOIN adempiere.ad_org as org ON (org.ad_org_id = pydma.ad_org_id)
+   INNER JOIN adempiere.ad_client as cli ON (org.ad_client_id = cli.ad_client_id)
+   INNER JOIN adempiere.ad_clientinfo as cliinfo ON (cli.ad_client_id = cliinfo.ad_client_id)
+   INNER JOIN adempiere.ad_orginfo as orginfo ON (org.ad_org_id = orginfo.ad_org_id)
+   LEFT JOIN adempiere.ad_image AS IMG ON (cliinfo.logoreport_id = IMG.ad_image_id)
+   INNER JOIN c_currency curr1 on pyr.c_currency_id = curr1.c_currency_id
+   LEFT JOIN c_currency_trl currt1 on curr1.c_currency_id = currt1.c_currency_id and currt1.ad_language = (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID=$P{AD_Client_ID}) 
+   LEFT JOIN c_currency curr2 on curr2.c_currency_id = (SELECT DISTINCT C_Currency_ID FROM c_acctschema WHERE C_AcctSchema_ID = $P{C_AcctSchema_ID})
+   LEFT JOIN c_currency_trl currt2 on curr2.c_currency_id = currt2.c_currency_id and currt2.ad_language = (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID=$P{AD_Client_ID})
+   WHERE  pydma.ad_client_id = $P{AD_Client_ID} 
+   		AND CASE WHEN ($P{AD_Org_ID} IS NULL OR $P{AD_Org_ID} = 0 OR $P{AD_Org_ID} = pydma.ad_org_id ) THEN 1=1 ELSE 1=0 END 
+   		AND CASE WHEN pydma.db_acct != 0 OR pydma.cr_acct != 0 THEN 1=1 ELSE 1=0 END
+		AND pydma.c_acctschema_id = $P{C_AcctSchema_ID} 
 		AND per.c_period_id >= $P{C_PeriodIni_ID} 
         AND per.c_period_id <= $P{C_PeriodEnd_ID} AND per.c_year_id = $P{C_Year_ID}
         AND pydma.optmode='A'   --	AND pydma.accounttype ='E'
