0739e5bfd89079d61dc481792d5f9639d3c5ed72
luisamesty <luisamesty@luisamesty@gmail.com>
2025-06-03 08:30:30 +0200

    release-11_LPY-Import Model Validator Dummy on Editor. To avoid circular reference

---
+++ org.amerpsoft.com.idempiere.editors-com/src/org/amerp/amxeditor/factory/ModelValidatorFactory.java
@@ -0,0 +1,14 @@
+package org.amerp.amxeditor.factory;
+
+import org.adempiere.base.IModelValidatorFactory;
+import org.compiere.model.ModelValidator;
+
+public class ModelValidatorFactory implements IModelValidatorFactory {
+
+	@Override
+	public ModelValidator newModelValidatorInstance(String className) {
+		//
+		return null;
+	}
+
+}
