f018b85c4ee1e16911d13bfdbcc9f92c55881988
Luis Amesty Linares <luisamesty@gmail.com>
2025-06-27 17:29:26 +0200

    release-12 renombrado c_elementvalue_id por bal_c_elementvalue_id. Ajustada Query

--- org.amerpsoft.com.idempiere.financial/reports.jasper/postgresql/TrialBalanceOnePeriod_TreeOrg_Fun/TrialBalanceOnePeriod_TreeOrg_Fun.sql
+++ org.amerpsoft.com.idempiere.financial/reports.jasper/postgresql/TrialBalanceOnePeriod_TreeOrg_Fun/TrialBalanceOnePeriod_TreeOrg_Fun.sql
@@ -50,7 +50,7 @@ FULL JOIN (
 			FROM amf_element_value_tree_extended($P{AD_Client_ID}, $P{C_AcctSchema_ID}) AS eve1
 			LEFT JOIN amf_org_tree($P{AD_Client_ID}, $P{AD_Org_ID}, $P{AD_OrgParent_ID}) AS org1 ON org1.org_ad_client_id = eve1.ad_client_id
 			LEFT JOIN amf_balance_account_org($P{AD_Client_ID}, $P{AD_Org_ID}, $P{C_AcctSchema_ID}, $P{C_Period_ID}, $P{PostingType}, NULL)
-				AS bal1 ON bal1.c_elementvalue_id = eve1.c_elementvalue_id AND bal1.ad_org_id = org1.org_ad_org_id
+				AS bal1 ON bal1.bal_c_elementvalue_id = eve1.c_elementvalue_id AND bal1.ad_org_id = org1.org_ad_org_id
 			WHERE eve1.issummary = 'N' AND ($P{isShowZERO} = 'Y' OR ($P{isShowZERO} = 'N' AND (
 							COALESCE(bal1.openbalance, 0) <> 0
 							OR COALESCE(bal1.amtacctdr, 0) <> 0
@@ -61,12 +61,11 @@ FULL JOIN (
 			FROM amf_element_value_tree_extended($P{AD_Client_ID}, $P{C_AcctSchema_ID}) AS eve2
 			LEFT JOIN amf_org_tree($P{AD_Client_ID}, $P{AD_Org_ID}, $P{AD_OrgParent_ID}) AS org2 ON org2.org_ad_client_id = eve2.ad_client_id
 			LEFT JOIN amf_balance_account_org($P{AD_Client_ID}, $P{AD_Org_ID}, $P{C_AcctSchema_ID}, $P{C_Period_ID}, $P{PostingType}, $P{C_ElementValue_ID})
-				AS bal2 ON bal2.c_elementvalue_id = eve2.c_elementvalue_id AND bal2.ad_org_id = org2.org_ad_org_id
+				AS bal2 ON bal2.bal_c_elementvalue_id = eve2.c_elementvalue_id AND bal2.ad_org_id = org2.org_ad_org_id
 			WHERE 
 			CASE WHEN ($P{C_ElementValue_ID} IS NOT NULL AND $P{C_ElementValue_ID} = eve2.c_elementvalue_id )  THEN 1=1 ELSE 1=0 END
 			AND eve2.issummary = 'N' 
 	) AS bal
-	
 ) AS balances ON 1=0
 WHERE header_info.imp_header = 1 OR 
 	(ad_client_id= $P{AD_Client_ID} 
