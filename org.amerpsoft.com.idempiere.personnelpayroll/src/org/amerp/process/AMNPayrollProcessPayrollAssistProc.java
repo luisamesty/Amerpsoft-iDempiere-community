ec0e765ba6d3d964fb70dd57aede248dcbd67ed3
luisamesty <luisamesty@luisamesty@gmail.com>
2025-07-15 09:23:40 +0200

    release-11_LPY Removida traza de prue en Clases de proceso de asistencia.

--- org.amerpsoft.com.idempiere.personnelpayroll/src/org/amerp/process/AMNPayrollProcessPayrollAssistProc.java
+++ org.amerpsoft.com.idempiere.personnelpayroll/src/org/amerp/process/AMNPayrollProcessPayrollAssistProc.java
@@ -110,7 +110,6 @@ public class AMNPayrollProcessPayrollAssistProc {
 		    if (isError) {
 		    	return locMsg_Value;
 		    }
-log.warning("PASO - Turno - Trabajador="+amnemployee.getValue()+" ShiftDetail="+amnshiftdetail.getName()+" Dayof Week= "+amnshiftdetail.getdayofweek());
 		    // Creates Shift_in1, Shift_in2, Shift_out1, shift_oou2 from entrytime, breakstart, timeout and breakminutes
 		    if (amnshiftdetail.getShift_In1()==null)
 		    	amnshiftdetail.setShift_In1(amnshiftdetail.getEntryTime());
@@ -183,7 +182,6 @@ log.warning("PASO - Turno - Trabajador="+amnemployee.getValue()+" ShiftDetail="+
 							break;		
 					}
 				}	
-log.warning("PASO - Eventos - Trabajador="+amnemployee.getValue()+" No Eventos="+AssistCounter+" assitAMN_Shift_ID= "+assitAMN_Shift_ID+ " p_AMN_Assist_Process_Mode="+p_AMN_Assist_Process_Mode);
 				// Verify for AssistCounter AND AMN_Shift_ID
 				if (AssistCounter == 0 ) {
 					assitAMN_Shift_ID = empAMN_Shift_ID;
@@ -198,7 +196,6 @@ log.warning("PASO - Eventos - Trabajador="+amnemployee.getValue()+" No Eventos="
 					// *   "1": Create or Update Records Only from  AMN_Payroll_Assist
 					// *   "2": Create or Update Records from  AMN_Payroll_Assist and Fill Default Values from AMN_Shift_Detail
 					// *   "3: Create or Update Records from Only from  AMN_Shift_Detail
-log.warning("PASO - MODO Seleccion - Trabajador="+amnemployee.getValue()+" No Eventos="+AssistCounter+" assitAMN_Shift_ID= "+assitAMN_Shift_ID+ " p_AMN_Assist_Process_Mode="+p_AMN_Assist_Process_Mode);
 					switch (p_AMN_Assist_Process_Mode) {
 						//  "0": Clean Records on AMN_Payroll_Assist_Proc 
 
@@ -214,7 +211,6 @@ log.warning("PASO - MODO Seleccion - Trabajador="+amnemployee.getValue()+" No Ev
 							break;
 						//   "1": Create or Update Records Only from  AMN_Payroll_Assist
 						case "1":
-log.warning("PASO - MODO 1 - Trabajador="+amnemployee.getValue()+" No Eventos="+AssistCounter+" assitAMN_Shift_ID= "+assitAMN_Shift_ID+ " p_AMN_Assist_Process_Mode="+p_AMN_Assist_Process_Mode);
 							//   "1": Create or Update Records Only from  AMN_Payroll_Assist
 							locMsg_Value=locMsg_Value+Msg.getElement(ctx, "AMN_Shift_ID")+":"+assitAMN_Shift_ID+" "+
 									Msg.getMsg(ctx,"Date")+":"+p_Event_Date.toString().substring(0,10)+" "+
@@ -229,7 +225,6 @@ log.warning("PASO - MODO 1 - Trabajador="+amnemployee.getValue()+" No Eventos="+
 									p_AMN_Employee_ID, p_Event_Date, assitAMN_Shift_ID,
 									Descanso, Excused ,
 									atthours, p_IsScheduled) ;
-log.warning("PASO - Crear Registro de AMN_payroll_asist - Trabajador="+amnemployee.getValue()+" Evento="+p_Event_Date);
 							break;
 						//  "2": Create or Update Records from  AMN_Payroll_Assist and Fill Default Values from AMN_Shift_Detail
 						case "2":
