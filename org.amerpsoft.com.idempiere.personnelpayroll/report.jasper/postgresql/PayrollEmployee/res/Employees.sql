17c3cf55b0554a26a4aee749aea5373451184428
luisamesty <luisamesty@luisamesty@gmail.com>
2026-02-19 22:28:16 +0100

    release-12_LPY-70 Reportes Jaspersoft ajustar exportación a Excel

--- org.amerpsoft.com.idempiere.personnelpayroll/report.jasper/postgresql/PayrollEmployee/res/Employees.sql
+++ org.amerpsoft.com.idempiere.personnelpayroll/report.jasper/postgresql/PayrollEmployee/res/Employees.sql
@@ -1,17 +1,14 @@
 -- Employees.jrxml
 -- Employees Report
--- Condicon agregada de Localidad Sector etc.
--- Status agregado
-SELECT  *
-FROM (
-	SELECT DISTINCT
-	-- REPORT HEADER
+SELECT DISTINCT *
+FROM (SELECT DISTINCT 
+-- REPORT HEADER
 	CASE WHEN ( $P{AD_Org_ID} = 0 OR $P{AD_Org_ID} IS NULL ) THEN '' ELSE COALESCE(orginfo.taxid,'') END as rep_taxid,
 	CASE WHEN ( $P{AD_Org_ID} = 0 OR $P{AD_Org_ID} IS NULL ) THEN img1.binarydata ELSE img2.binarydata END as rep_logo,
     CASE WHEN ( $P{AD_Org_ID} = 0 OR $P{AD_Org_ID} IS NULL ) THEN concat(COALESCE(cli.name,cli.value),' - Consolidado') ELSE coalesce(org.name,org.value,'') END as rep_name,
-	-- CLIENT
+-- CLIENT
 	emp.ad_client_id,
-	-- ORGANIZATION
+-- ORGANIZATION
 	emp.ad_orgto_id,
     coalesce(org.value,'') as org_value,
     coalesce(org.name,org.value,'') as org_name,
@@ -21,7 +18,6 @@ FROM (
 -- BPARTNER
 	 COALESCE(cbp.taxid, cbp.amerp_rifseniat) as rif,
 -- EMPLOYEE 
-	emp.amn_employee_id,
     emp.name as nombre,
     COALESCE(emp.lastname1,'') AS apellido1,
     COALESCE(emp.lastname2,'') AS apellido2,
@@ -40,51 +36,62 @@ FROM (
          ELSE (DATE_PART('year', current_date::date) - DATE_PART('year', emp.birthday::date))
     END as edad, 
     emp.bloodtype as tipo_sangre, emp.bloodrh as tipo_rh, emp.birthplace as lugar_nacimiento, 
-    emp.sex as sexo,
     (SELECT rlt.name
 		 FROM adempiere.ad_reference as rfr
 		 INNER JOIN adempiere.ad_ref_list as rfl ON (rfl.ad_reference_id = rfr.ad_reference_id)
 		 INNER JOIN adempiere.ad_ref_list_trl as rlt ON (rlt.ad_ref_list_id = rfl.ad_ref_list_id)
-		 WHERE LOWER(rfr.name) = 'amn_sex' AND rlt.ad_language= (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID = $P{AD_Client_ID})  and rfl.value= emp.sex) as sexol,
+		 WHERE rfr.name = 'amn_sex' AND rlt.ad_language= 'es_VE' and rfl.value= emp.sex) as sexo,
     (SELECT rlt.name
 		 FROM adempiere.ad_reference as rfr
 		 INNER JOIN adempiere.ad_ref_list as rfl ON (rfl.ad_reference_id = rfr.ad_reference_id)
 		 INNER JOIN adempiere.ad_ref_list_trl as rlt ON (rlt.ad_ref_list_id = rfl.ad_ref_list_id)
-		 WHERE LOWER(rfr.name) = 'amn_civilstatus' AND rlt.ad_language= (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID = $P{AD_Client_ID})  and rfl.value= emp.civilstatus) as estado_civil, 
+		 WHERE rfr.name = 'amn_civilstatus' AND rlt.ad_language= 'es_VE' and rfl.value= emp.civilstatus) as estado_civil, 
     (SELECT rlt.name
 		 FROM adempiere.ad_reference as rfr
 		 INNER JOIN adempiere.ad_ref_list as rfl ON (rfl.ad_reference_id = rfr.ad_reference_id)
 		 INNER JOIN adempiere.ad_ref_list_trl as rlt ON (rlt.ad_ref_list_id = rfl.ad_ref_list_id)
-		 WHERE LOWER(rfr.name) = 'amn_spouse' AND rlt.ad_language= (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID = $P{AD_Client_ID})  and rfl.value= emp.spouse) as conyuge,
+		 WHERE rfr.name = 'amn_spouse' AND rlt.ad_language= 'es_VE' and rfl.value= emp.spouse) as conyuge,
     (SELECT rlt.name
 		 FROM adempiere.ad_reference as rfr
 		 INNER JOIN adempiere.ad_ref_list as rfl ON (rfl.ad_reference_id = rfr.ad_reference_id)
 		 INNER JOIN adempiere.ad_ref_list_trl as rlt ON (rlt.ad_ref_list_id = rfl.ad_ref_list_id)
-		 WHERE LOWER(rfr.name) = 'amn_handuse' AND rlt.ad_language= (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID = $P{AD_Client_ID})  and rfl.value= emp.handuse) as mano_dominante, 
-    emp.isstudying as estudia, 
-    emp.ismedicated as usa_medicamentos,
-    emp.uselenses as lentes, 
+		 WHERE rfr.name = 'amn_handuse' AND rlt.ad_language= 'es_VE' and rfl.value= emp.handuse) as mano_dominante, 
     (SELECT rlt.name
 		 FROM adempiere.ad_reference as rfr
 		 INNER JOIN adempiere.ad_ref_list as rfl ON (rfl.ad_reference_id = rfr.ad_reference_id)
 		 INNER JOIN adempiere.ad_ref_list_trl as rlt ON (rlt.ad_ref_list_id = rfl.ad_ref_list_id)
-		 WHERE LOWER(rfr.name) = 'amn_educationalgrade' AND rlt.ad_language= (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID = $P{AD_Client_ID})  and rfl.value= emp.educationgrade) as grado_instruccion, 
+		 WHERE rfr.name = 'amn_spouse' AND rlt.ad_language= 'es_VE' and rfl.value= emp.isstudying) as estudia, 
+    (SELECT rlt.name
+		 FROM adempiere.ad_reference as rfr
+		 INNER JOIN adempiere.ad_ref_list as rfl ON (rfl.ad_reference_id = rfr.ad_reference_id)
+		 INNER JOIN adempiere.ad_ref_list_trl as rlt ON (rlt.ad_ref_list_id = rfl.ad_ref_list_id)
+		 WHERE rfr.name = 'amn_spouse' AND rlt.ad_language= 'es_VE' and rfl.value= emp.ismedicated) as usa_medicamentos,
+    (SELECT rlt.name
+		 FROM adempiere.ad_reference as rfr
+		 INNER JOIN adempiere.ad_ref_list as rfl ON (rfl.ad_reference_id = rfr.ad_reference_id)
+		 INNER JOIN adempiere.ad_ref_list_trl as rlt ON (rlt.ad_ref_list_id = rfl.ad_ref_list_id)
+		 WHERE rfr.name = 'amn_spouse' AND rlt.ad_language= 'es_VE' and rfl.value= emp.uselenses) as lentes, 
+    (SELECT rlt.name
+		 FROM adempiere.ad_reference as rfr
+		 INNER JOIN adempiere.ad_ref_list as rfl ON (rfl.ad_reference_id = rfr.ad_reference_id)
+		 INNER JOIN adempiere.ad_ref_list_trl as rlt ON (rlt.ad_ref_list_id = rfl.ad_ref_list_id)
+		 WHERE rfr.name = 'amn_educationalgrade' AND rlt.ad_language= 'es_VE' and rfl.value= emp.educationgrade) as grado_instruccion, 
     emp.educationlevel as nivel_instruccion, emp.profession as profesion,
     (SELECT rlt.name
 		 FROM adempiere.ad_reference as rfr
 		 INNER JOIN adempiere.ad_ref_list as rfl ON (rfl.ad_reference_id = rfr.ad_reference_id)
 		 INNER JOIN adempiere.ad_ref_list_trl as rlt ON (rlt.ad_ref_list_id = rfl.ad_ref_list_id)
-		 WHERE LOWER(rfr.name) = 'amn_jobcondition' AND rlt.ad_language= (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID = $P{AD_Client_ID})  and rfl.value= emp.jobcondition) as condicion_trabajo, 
+		 WHERE rfr.name = 'amn_jobcondition' AND rlt.ad_language= 'es_VE' and rfl.value= emp.jobcondition) as condicion_trabajo, 
     (SELECT rlt.name
 		 FROM adempiere.ad_reference as rfr
 		 INNER JOIN adempiere.ad_ref_list as rfl ON (rfl.ad_reference_id = rfr.ad_reference_id)
 		 INNER JOIN adempiere.ad_ref_list_trl as rlt ON (rlt.ad_ref_list_id = rfl.ad_ref_list_id)
-		 WHERE LOWER(rfr.name) = 'amn_paymenttype' AND rlt.ad_language= (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID = $P{AD_Client_ID})  and rfl.value= emp.paymenttype) as tipo_pago, 
+		 WHERE rfr.name = 'amn_paymenttype' AND rlt.ad_language= 'es_VE' and rfl.value= emp.paymenttype) as tipo_pago, 
     (SELECT rlt.name
 		 FROM adempiere.ad_reference as rfr
 		 INNER JOIN adempiere.ad_ref_list as rfl ON (rfl.ad_reference_id = rfr.ad_reference_id)
 		 INNER JOIN adempiere.ad_ref_list_trl as rlt ON (rlt.ad_ref_list_id = rfl.ad_ref_list_id)
-		 WHERE LOWER(rfr.name) = 'amn_payrollmode' AND rlt.ad_language= (SELECT AD_Language FROM AD_Client WHERE AD_Client_ID = $P{AD_Client_ID})  and rfl.value= emp.payrollmode) as tipo_nomina, 
+		 WHERE rfr.name = 'amn_payrollmode' AND rlt.ad_language= 'es_VE' and rfl.value= emp.payrollmode) as tipo_nomina, 
     cok.OK_salary, emp.salary as sueldo, emp.incomedate as fecha_ingreso, emp.Birthday as fecha_nacimiento,
     date_part('year',age(current_date, emp.incomedate)) a_servicio, 
     date_part('month',age(current_date, emp.incomedate)) m_servicio, 
@@ -126,22 +133,10 @@ FROM (
     loc_n.value AS loc_value ,
     COALESCE(loc_n.name, loc_n.description)  AS loc_name,
  -- SECTOR (NOMINA)
-    COALESCE(sector.value,'N/D') AS sector_value ,
-    COALESCE(sector.name, sector.description,'N/D') AS sector_name,
+    sector.value AS sector_value ,
+    COALESCE(sector.name, sector.description) AS sector_name,
  -- DEPENDENT (CORREO E)
-    usr.email as correoe,
-        emp.status, 
-    CASE
-		WHEN ($P{AMN_Status_A} = 'Y'
-		AND emp.status = 'A') THEN 1
-		WHEN ($P{AMN_Status_V} = 'Y'
-		AND emp.status = 'V') THEN 1
-		WHEN ($P{AMN_Status_R} = 'Y'
-		AND emp.status = 'R') THEN 1
-		WHEN ($P{AMN_Status_S} = 'Y'
-		AND emp.status = 'S') THEN 1
-		ELSE 0
-	END AS imprimir_status
+    usr.email as correoe
 FROM adempiere.amn_employee as emp
 INNER JOIN adempiere.c_bpartner as cbp ON cbp.c_bpartner_id = emp.c_bpartner_id 
 INNER JOIN adempiere.ad_client as cli ON (cbp.ad_client_id = cli.ad_client_id)
@@ -176,18 +171,12 @@ INNER JOIN adempiere.ad_orginfo as orginfo ON (org.ad_org_id = orginfo.ad_org_id
  INNER JOIN adempiere.amn_jobtitle as job ON (job.amn_jobtitle_id= emp.amn_jobtitle_id)
  LEFT JOIN adempiere.amn_jobstation as job_s ON (job_s.amn_jobstation_id= job.amn_jobstation_id)
  INNER JOIN adempiere.amn_location as loc_n ON (loc_n.amn_location_id= emp.amn_location_id)
- LEFT JOIN adempiere.amn_sector as sector ON sector.amn_sector_id = emp.amn_sector_id
+ INNER JOIN adempiere.amn_sector as sector ON sector.amn_sector_id = emp.amn_sector_id
  LEFT JOIN adempiere.amn_dependent as dep ON (dep.amn_employee_id= emp.amn_employee_id)
  WHERE emp.isactive= 'Y' AND
        emp.ad_client_id=  $P{AD_Client_ID} 
        AND ( CASE WHEN ( ( $P{AD_Org_ID} = 0 OR $P{AD_Org_ID} IS NULL ) OR emp.ad_orgto_id= $P{AD_Org_ID} ) THEN 1=1 ELSE 1=0 END )
 ) as trabajadores
-WHERE imprimir_status = 1 AND imprimir_nom= 1 AND imprimir_emp= 1
+WHERE imprimir_nom= 1 AND imprimir_emp= 1
 AND imprimir_org=1 AND ad_client_id = $P{AD_Client_ID}
---ORDER BY org_value, tipo_contrato, loc_value, sector_value, codigo, nombre ASC
-ORDER BY
-    CASE WHEN $P{isShowOrganization} = 'Y' 	THEN org_value 		ELSE NULL END,
-    CASE WHEN $P{isShowContract} = 'Y' 		THEN tipo_contrato 	ELSE NULL END,
-    CASE WHEN $P{isShowLocation} = 'Y' 		THEN loc_value      ELSE NULL END,
-    CASE WHEN $P{isShowSector} = 'Y' 		THEN sector_value   ELSE NULL END,
-    codigo, nombre
+ORDER BY org_value, tipo_contrato, loc_value, sector_value, codigo, nombre ASC
